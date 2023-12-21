89f51d36db13b5b2dd8dbfe031ae7e349aa91ab8
Akifrm <61554024+Akifrm@users.noreply.github.com>
2023-12-21 17:47:44 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ window.addEventListener("scroll", function () {
     const element = document.getElementsByTagName('header')[0];
     if (scrollY > 0) {
         element.style.opacity = .9;
+        element.style.backgroundColor = 'white';
     } else {
         element.style.opacity = 1;
+        element.style.backgroundColor = null;
     }
 });
