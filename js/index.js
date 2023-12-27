8c873810839330480ed052210e7d5346fd8714e5
Akifrm <61554024+Akifrm@users.noreply.github.com>
2023-12-27 12:19:04 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,3 +8,23 @@ window.addEventListener("scroll", function () {
         element.style.backgroundColor = null;
     }
 });
+
+function menuOpen() {
+    const menu = document.getElementById('menu');
+    const button = document.getElementById('button');
+    const button2 = document.getElementById('button2');
+
+    button.style.display = 'none';
+    button2.style.display = 'inline';
+    menu.classList.remove('active');
+}
+
+function menuClose() {
+    const menu = document.getElementById('menu');
+    const button = document.getElementById('button');
+    const button2 = document.getElementById('button2');
+
+    button.style.display = 'inline';
+    button2.style.display = 'none';
+    menu.classList.add('active');
+}
