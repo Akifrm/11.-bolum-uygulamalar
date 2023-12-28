c78cd1b47bd3a923cb97f1f506dacbd3b68ba8ad
Akifrm <61554024+Akifrm@users.noreply.github.com>
2023-12-28 09:45:32 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 window.addEventListener("scroll", function () {
     const element = document.getElementsByTagName('header')[0];
+    const menu = document.getElementById('menu');
     if (scrollY > 0) {
         element.style.opacity = .9;
         element.style.backgroundColor = 'white';
     } else {
-        element.style.opacity = 1;
-        element.style.backgroundColor = null;
+        if (menu.classList.item(0) === 'active') {
+            element.style.opacity = 1;
+            element.style.backgroundColor = null;
+        }
     }
 });
 
@@ -13,18 +16,24 @@ function menuOpen() {
     const menu = document.getElementById('menu');
     const button = document.getElementById('button');
     const button2 = document.getElementById('button2');
+    const element = document.getElementsByTagName('header')[0];
 
     button.style.display = 'none';
     button2.style.display = 'inline';
     menu.classList.remove('active');
+    element.style.opacity = .9;
+    element.style.backgroundColor = 'white';
 }
 
 function menuClose() {
     const menu = document.getElementById('menu');
     const button = document.getElementById('button');
     const button2 = document.getElementById('button2');
+    const element = document.getElementsByTagName('header')[0];
 
     button.style.display = 'inline';
     button2.style.display = 'none';
     menu.classList.add('active');
+    element.style.opacity = 1;
+    element.style.backgroundColor = null;
 }
